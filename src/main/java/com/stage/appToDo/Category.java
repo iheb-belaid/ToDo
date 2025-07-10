abcd4e408ca3343e386b6ea22febfc8a6697abf3
iheb belaid <ihebbelaid3@gmail.com>
2025-07-10 09:32:35 +0100

    test build auto via webhook

--- src/main/java/com/stage/appToDo/Category.java
+++ src/main/java/com/stage/appToDo/Category.java
@@ -13,6 +13,8 @@ public class Category {
     @NotBlank
     private String name;
 
+    /*----test auto build------*/
+
     /* ---------- getters / setters ---------- */
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
